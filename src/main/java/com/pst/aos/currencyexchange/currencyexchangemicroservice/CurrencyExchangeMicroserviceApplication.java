c926aa6647790f0dd5c1a3a789c98f59674045f5
unknown <teodoramaria0112@gmail.com>
2019-01-06 20:47:26 +0200

    Initial commit

---
+++ src/main/java/com/pst/aos/currencyexchange/currencyexchangemicroservice/CurrencyExchangeMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.pst.aos.currencyexchange.currencyexchangemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangeMicroserviceApplication.class, args);
+    }
+
+}
+
